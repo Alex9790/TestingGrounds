ac6679e7d290fb47f496f79e63b4a415912530a1
Darkestt <martinez.alex.9790@gmail.com>
2020-01-04 16:00:22 -0400

    248. Se crea nueva clase C++ PatrolRoute ActorComponent que reempla a PatrollingGuard para almacenar la lista de Patrol Points, esto cn la finalidad de generalizar el acceso a la lista de Patrol Points sin importar la clase

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -4,23 +4,33 @@
 #include "BehaviorTree/BlackboardComponent.h"
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" //TODO: remove coupling, elimina cuando se aplique solucion mas general
-
+#include "PatrolRoute.h" 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {	
 //////PRIMER NODO GET PATROL POINTS
-	//se obtienen los patrol points
+	//se obtienen los patrol route
 	auto AIController = OwnerComp.GetAIOwner();
 	auto ControlledPawn = AIController->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	//se protege en caso de no tener PatrolRoute Cmponent
+	if(!ensure(PatrolRoute)){ return EBTNodeResult::Failed; }
+
 	//se castea el Pawn a Patrolling Guard para obtener referencia a los Patrol Points
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->GetPatrolPointsCPP();
+	//auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	//auto PatrolPoints = PatrollingGuard->GetPatrolPointsCPP();
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPointsCPP();
+	//se alerta en caso de no tener Patrol Points asignados
+	if (PatrolPoints.Num() == 0) {
+		UE_LOG(LogTemp, Warning, TEXT("No se han asignado Patrol Points."))
+		return EBTNodeResult::Failed;
+	}
 
 //////SEGUNDO NODO SET NEXT WAYPOINT
 	//se toma referecia del blackboard relacionado
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
-	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	//TODO: proteger en caso de PatrolPoins == null
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);	
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
 //////TERCER NODO CYCLE INDEX
